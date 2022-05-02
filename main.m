5d2734980cb7d9ef0d601e2776fd4a556d2a21f6
Cem GEÇGEL <gecgelcem@outlook.com>
2022-05-02 22:01:18 +0300

    Getting n

--- main.m
+++ main.m
@@ -9,4 +9,9 @@ file = printer('Output.txt');
 file.print("gecgelcem 02.05.2022");
 file.print("me425 spring2022 prj");
 
-file.print("Your input: %.0f", ask("Give a digit: ", 0, 9));
+n = ask(sprintf("\nEnter %1s in [%1.0f, %1.0f] %0s: ", "n", 2, 5, ""), 2, 5);
+
+file.print("");
+file.print("Input:");
+file.print("~~~~~~");
+file.print("[-] %1s = %1.0f %3s", "n", n, "");
